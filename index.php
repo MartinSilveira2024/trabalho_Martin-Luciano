acf3b2cccedd417915c24a0b82eef3a5fbe90d2d
LKubner <luciano.2022310952@aluno.iffar.edu.br>
2024-07-20 11:58:57 -0300

    alterei o index.php
    formulario de login e tudo mais

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manter Perfil</title>
 </head>
+
 <body>
-    <h1> Bem vindo zé </h1>
+    <h1> Bem vindo ao visualizador de perfil </h2>
+    <form action="login.php" method="post">
+        <label> Email: <input type="email" name="email"> <br></lable>
+            <label> Senha: <input type="password" name="senha"> </label> <br>
+            <input type="submit" value="Logar"> <br>
+    </form> <br> <br>
+    <a href="form-cadastro.php"> Cadastrar-se </a> <br> <br>
+    <a href="form-recuperar-senha.php"> Recuperar senha </a> <br>
 </body>
+
 </html>
