b8b5bd7bb57a32de74971833d0dfe2384519477a
Martín Silveira de Almeida <martin.2022311000@aluno.iffar.edu.br>
2024-07-20 12:28:17 -0300

    Update cadastrar.php

--- recuperar_senha/cadastrar.php
+++ recuperar_senha/cadastrar.php
@@ -9,12 +9,6 @@ $senha = $_POST['senha'];
 $sql = "INSERT INTO usuario (nome, email, senha) VALUES ('$nome','$email','$senha')";
 $resultado = mysqli_query($conexao, $sql);
 
-$consulta = $conexao -> query("SELECT email FROM usuario WHERE email = '$email'");
-$exibe = $consulta ->fetch(PDO::FETCH_ASSOC);
-if($consulta -> rowCount() == 1) {
-    echo "email já cadastrado";
-}
-else {
 if ($resultado === false) {
         if(mysqli_errno($conexao) == 1062){
             echo "email já cadastrado no sistema! tente fazer o login ou faça a recuperação de senha.";
@@ -22,6 +16,6 @@ if ($resultado === false) {
         } else {
     echo "Erro ao inserir o novo usuario!" .
         mysqli_errno($conexao) . ": " . mysqli_error($conexao);
-} }
 }
-header("Location: index.php");
+}
+header("Location: ./index.php");
