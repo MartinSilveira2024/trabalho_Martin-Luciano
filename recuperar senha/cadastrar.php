840372216a3e8b562e8dfed5aaf2bd984de19e81
Martín Silveira de Almeida <martin.2022311000@aluno.iffar.edu.br>
2024-07-20 12:07:32 -0300

    não permitir cadastrar mesmo email

--- recuperar senha/cadastrar.php
+++ recuperar senha/cadastrar.php
@@ -8,6 +8,13 @@ $senha = $_POST['senha'];
 //Tem que armazenar a senha de modo seguro depois
 $sql = "INSERT INTO usuario (nome, email, senha) VALUES ('$nome','$email','$senha')";
 $resultado = mysqli_query($conexao, $sql);
+
+$consulta = $cn -> query("SELECT email FROM usuario WHERE email = '$email'");
+$exibe = $consulta ->fetch(PDO::FETCH_ASSOC);
+if($consulta -> rowCount() == 1) {
+    echo "email já cadastrado";
+}
+else {
 if ($resultado === false) {
         if(mysqli_errno($conexao) == 1062){
             echo "email já cadastrado no sistema! tente fazer o login ou faça a recuperação de senha.";
@@ -15,6 +22,6 @@ if ($resultado === false) {
         } else {
     echo "Erro ao inserir o novo usuario!" .
         mysqli_errno($conexao) . ": " . mysqli_error($conexao);
-}
+} }
 }
 header("Location: index.php");
