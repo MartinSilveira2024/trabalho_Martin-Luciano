b7dbbfdde47c92991e2b77595e10ffba374966e1
Martín Silveira de Almeida <martin.2022311000@aluno.iffar.edu.br>
2024-07-22 15:55:29 -0300

    fazendo o index quando o usuario loga

--- recuperar_senha/principal.php
+++ recuperar_senha/principal.php
@@ -1,3 +1,16 @@
+<?php
+$conexao = mysqli_connect("localhost", "root", "", "trab_martin_luciano");
+$sql = "SELECT * FROM usuario";
+$resultado = mysqli_query($conexao, $sql);
+if ($resultado != false) {
+    $arquivos = mysqli_fetch_all($resultado, MYSQLI_BOTH);
+} else {
+    echo "Erro ao executar comando SQL.";
+    die();
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +19,36 @@
     <title>Pagina principal</title>
 </head>
 <body>
+
+<table>
+        <thead>
+            <tr>
+                <th colspan="2"> Nome</th>
+                <th colspan="2"> Opções </th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+                    foreach ($arquivos as $arquivo) {
+                        $arq = $arquivo['nome'];
+                        echo "<tr>"; //iniciar a linha
+                        echo "<td>" . $arq = $arquivo['nome']; "</td>";
+
+
+                        //echo "<td><button onclick='excluir(\"" . $arquivo['Nome_arquivo'] . "\");'>Excluir</button></td></tr>";
+                    }
+                    ?> </tbody>
+    </table>
+
+    <script>
+        function excluir(Nome_arquivo) {
+            confirm("Você tem certeza que deseja excluir este arquivo " + Nome_arquivo + "?");
+            let deletar = confirm("Você tem certeza que deseja excluir?")
+            if (deletar == true) {
+                window.location.href = "deletar.php?Nome_arquivo=" + Nome_arquivo;
+            }
+        }
+    </script>
     <h1> Você está logado! </h1>
 </body>
 </html>
