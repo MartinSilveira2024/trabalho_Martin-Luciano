882052d4027c6bb79b623a8baf94767c29f2e17e
LKubner <luciano.2022310952@aluno.iffar.edu.br>
2024-08-05 16:34:00 -0300

    amostradinho

--- usuario.sql
+++ usuario.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 20-Jul-2024 às 15:08
+-- Tempo de geração: 05-Ago-2024 às 19:32
 -- Versão do servidor: 8.0.31
 -- versão do PHP: 8.0.26
 
@@ -33,9 +33,22 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `nome` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `senha` varchar(255) NOT NULL,
-  `foto` varchar(255) NOT NULL,
-  PRIMARY KEY (`id_usuario`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `foto` varchar(255) NOT NULL DEFAULT 'default.jpg',
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `nome`, `email`, `senha`, `foto`) VALUES
+(1, 'Luciano Maia Kubner', 'lucianoteste@gmail.com', '123', 'default.jpg'),
+(2, 'teste', 'teste@gmail.com', '123', 'default.jpg'),
+(3, 'Luciano', 'luciano@123', '123', 'default.jpg'),
+(6, 'martin', 'martin@234', '123', 'default.jpg'),
+(7, 'jonas', 'jonas@gmail.com', '123', 'default.jpg'),
+(8, 'lucianoo', 'luciano.2022310952@aluno.iffar.edu.br', '123', '66b1280b7fccc.jfif');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
